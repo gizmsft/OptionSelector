8bf81c706a3005b21cf9913ed109b6d4ebf4c52c
aahmed <ayyaz_ahmed@hotmail.com>
2022-05-24 23:26:03 -0400

    saved

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-const { src, dest } = require('glup');
+const { src, dest } = require('gulp');
 const sassPlugin = require('gulp-sass');
-const uglifyPlugin = require('glup-uglify');
+const uglifyPlugin = require('gulp-uglify');
 
 var minifyCss = function (callback) {
-    src('css/*.css')
+    src('./css/*.css')
         .pipe(sassPlugin().on('error', sassPlugin.logError))
-        .pipe(dest('dist/css'));
+        .pipe(dest('./dist/css'));
 
     callback();
 }
 
 var minifyJs = function (callback) {
-    src('lib/*.js')
+    src('./lib/*.js')
         .pipe(uglifyPlugin().on('error', uglifyPlugin.logError))
-        .pipe(dest('dist/css'));
+        .pipe(dest('./dist/js'));
 
     // place code for your default task here
     callback();
